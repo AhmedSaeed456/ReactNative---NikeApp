d34f4765ebc26beb320fa59744ddfdd64d7ca512
kishk <mrsaytama000@gmail.com>
2023-06-13 18:35:23 +0300

    ProductScreen and details screen added

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View, Image, FlatList } from "react-native";
+import products from "../data/products";
+
+const ProductScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={{ width: "50%", padding: 1 }}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
+export default ProductScreen;
